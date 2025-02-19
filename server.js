1e6ec7686c3c89e9514a9540c5c836f394a31886
kicheol83 <suyikim7@gmail.com>
2025-02-20 01:10:28 +0900

    feat: develop author router ejs page

--- server.js
+++ server.js
@@ -7,22 +7,37 @@ const http = require("http");
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+const fs = require("fs");
+
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+  if (err) {
+    console.log("ERROR:", err);
+  } else {
+    user = JSON.parse(data);
+  }
+});
 
 // 2: Session
 
 // 3 => BCCR => Views code
-app.set("views", "views");
+app.set("views", "views"); 
 app.set("view engine", "ejs");
 
 // 4 Routing code
 app.post("/create-item", (req, res) => {
-    console.log(req);
-    res.json({test: "succes"});
+  console.log(req);
+  res.json({ test: "succes" }); 
+});
+
+app.get("/author", (req, res) => {
+  res.render("author", { user: user });
 });
 
 app.get("/", function (req, res) {
-    res.render("harid");
+  res.render("harid");
 });
+
 const server = http.createServer(app);
 let PORT = 3000;
 server.listen(PORT, function () {
