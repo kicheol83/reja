8ceb33cd1d973365b993db983518b1b8e0d558ce
kicheol83 <suyikim7@gmail.com>
2025-02-24 00:25:01 +0900

    feat: Loop or Callback function

--- train.js
+++ train.js
@@ -1,2 +1,36 @@
-console.log("TRAIN AREA");
+// console.log("TRAIN AREA");
 // TASK
+
+console.log("Jack Ma maslahatlari");
+const list = [
+  "yaxshi talaba boling", //0-20
+  "togri boshliq tanlang va koproq xato qiling", // 20-30
+  "ozingizni ishlashingizni boshlang", // 30-40
+  "siz kuchli bolgan narsalarni qiling", // 40-50
+  "yoshlarga investitsiya qiling", // 50-60
+  "endi dam oling, foydasi yoq endi", // 60
+];
+
+function maslahatBering(a, callback) {
+  if (typeof a !== "number") callback("insert a number", null);
+  else if (a <= 20) callback(null, list[0]);
+  else if (a > 20 && a <= 30) callback(null, list[1]);
+  else if (a > 30 && a <= 40) callback(null, list[2]);
+  else if (a > 40 && a <= 50) callback(null, list[3]);
+  else if (a > 50 && a <= 60) callback(null, list[4]);
+  else {
+    setTimeout(function () {
+        callback(null, list[5]);
+    }, 5000);
+    // callback(null, list[5]);
+  }
+}
+console.log("passed here 0");
+maslahatBering(65, (err, data) => {
+  if (err) console.log("EROR:", err);
+  else {
+    console.log("javob", data);
+  };
+});
+console.log("passed here 1");
+
