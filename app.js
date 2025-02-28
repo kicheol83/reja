736ebd9cadb0580a7d32defb371048e3f5e87b13
kicheol83 <suyikim7@gmail.com>
2025-03-01 02:11:28 +0900

    feat: FrontEnd javascriptni qurish

--- app.js
+++ app.js
@@ -33,12 +33,8 @@ app.post("/create-item", (req, res) => {
   console.log(req.body);
   const new_reja = req.body.reja;
   db.collection("plans").insertOne({ reja: new_reja }, (err, data) => {
-    if (err) {
-      console.log(err);
-      res.send("something went wrong");
-    } else {
-      res.send("succesfully added");
-    }
+    console.log(data.ops);
+    res.json(data.ops[0]);
   });
 });
 
